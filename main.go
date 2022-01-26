0b5e7895b9e7545109645b0d198553be22bb2b4a
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-01-26 22:27:52 +0900

    parsing commands

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	explorer "github.com/Younkyum/nomadcoin/explorer/templates"
-	"github.com/Younkyum/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to YK Coin\n\n")
+	fmt.Printf("Pleas use the following commads:\n\n")
+	fmt.Printf("exploere: Start the HTML Explorer\n")
+	fmt.Printf("rest:     Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }

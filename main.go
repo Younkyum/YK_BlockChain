1bb5758da97db7ba539dbc3a3c47843ed39b56f6
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-01-27 17:13:31 +0900

    flag

--- main.go
+++ main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to YK Coin\n\n")
-	fmt.Printf("Pleas use the following commads:\n\n")
-	fmt.Printf("exploere: Start the HTML Explorer\n")
-	fmt.Printf("rest:     Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
+import "github.com/Younkyum/nomadcoin/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }

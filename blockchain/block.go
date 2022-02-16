382b64c6728e3d4a2496d2d930f61f29a7047f09
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-02-16 17:53:35 +0900

    mining block

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/Younkyum/nomadcoin/db"
 	"github.com/Younkyum/nomadcoin/utils"
@@ -40,15 +41,29 @@ func FindBlock(hash string) (*Block, error) {
 	return block, nil
 }
 
+func (b *Block) mine() {
+	target := strings.Repeat("0", b.Difficutly)
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(fmt.Sprint(b))))
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string, prevHash string, height int) *Block {
 	block := &Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficutly: difficulty,
+		Nonce:      0,
 	}
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.mine()
 	block.persist()
 	return block
 }
--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/Younkyum/nomadcoin/cli"
+	"github.com/Younkyum/nomadcoin/db"
 )
 
 func main() {
-	difficutly := 6
-	target := strings.Repeat("0", difficutly)
-	nounce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nounce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNounce:%d\n\n", hash, target, nounce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nounce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
